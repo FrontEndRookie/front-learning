0f684f142565b50d53382b9dd1865195b4c8f7e2
XuMingHui <xuminghui@17dc.com>
2024-07-29 18:53:20 +0800

    feat: change

--- node学习/new learn/buffer/operate.js
+++
@@ -1,9 +0,0 @@
-let buffer =  Buffer.from([105,108,111,118,101,121,111,117]);
-console.log(buffer.toString());
-
-let bufferOver = Buffer.from('hello')
-bufferOver[0] = 361 // 舍弃高位数字 0001 0110 1001 =》 0110 1001 =》69
-console.log(bufferOver)
-
-let bufferChina = Buffer.from('中文')
-console.log(bufferChina) //打印出六个字节 e4 b8 ad e6 96 8
--- node学习/new learn/fs/文件夹操作.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-fs.mkdir('./mkdir',err=>{
-    if(err){
-        console.log(err)
-        return
-    }
-    
-})
