0e011b80be1df3f33e3eac1d409cd05473effe60
XuMingHui <xuminghui@17dc.com>
2025-03-03 16:08:38 +0800

    feat: 25.3.3

--- javascript/深拷贝及数组方法重写/jsonclone-problem.js
+++ javascript/深拷贝及数组方法重写/jsonclone-problem.js
@@ -4,7 +4,7 @@ let b = { a: a };
 a.b = b;
 JSON.parse(JSON.stringify(b)); //TypeError:Converting circular structure to JSON
 
-// 函数 undefined Symbol会跳过 nan会变成null
+// 函数 undefined Symbol会跳过 NaN,infinity会变成null Date对象会变成字符串 Map/Set/Error/RegExp 会变成空对象 BigInt会报错
 let c = {
   a() {},
   b: NaN,
